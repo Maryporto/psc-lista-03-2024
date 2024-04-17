28797096c1701efc62e663b1498827fb7ca7e71e
Mariana Souza Porto <160690963+Maryporto@users.noreply.github.com>
2024-04-16 22:49:51 -0300

    calculadora.java

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class calculadora {
+import java.util.Scanner;
+
+public class calculadora {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número:");
+        double numero1 = entrada.nextDouble();
+
+        System.out.print("Digite o segundo número:");
+        double numero2 = entrada.nextDouble();
+
+        System.out.print("Digite o símbolo da operaç?o (+, -, *, /, ^):");
+        char operacao = entrada.next().charAt(0);
+
+        double resultado = 0;
+
+        switch (operacao) {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                resultado = numero1 / numero2;
+                break;
+            case '^':
+                resultado = Math.pow(numero1, numero2);
+                break;
+            default:
+                System.out.println("Erro: Símbolo de operaç?o inválido.");
+                return;
+        }
+
+        System.out.println("Resultado da operaç?o: " + resultado);
+
+        entrada.close();
+    }
 }
