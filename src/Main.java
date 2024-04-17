3f00aa4ff77c76367a868bffd408ac229ba00c39
Mariana Souza Porto <160690963+Maryporto@users.noreply.github.com>
2024-04-16 22:43:37 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = entrada.nextDouble();
+        
+        System.out.print("Digite o segundo número: ");
+        double numero2 = entrada.nextDouble();
+        
+        System.out.print("Digite o terceiro número: ");
+        double numero3 = entrada.nextDouble();
+        
+        double maior = numero1;
+        if (numero2 > maior) {
+            maior = numero2;
         }
+        if (numero3 > maior) {
+            maior = numero3;
+        }
+        
+        double menor = numero1;
+        if (numero2 < menor) {
+            menor = numero2;
+        }
+        if (numero3 < menor) {
+            menor = numero3;
+        }
+        
+        double media = (numero1 + numero2 + numero3) / 3;
+        
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+        System.out.println("A média aritmética dos números é: " + media);
+        
+        entrada.close();
     }
 }
